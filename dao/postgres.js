61f00fadcd7ebbeb8f5a2d03548d24b99e48c00e
igcing <icuevasi@everis.com>
2021-04-10 01:55:46 -0400

    v0.0.4 test extra ssl

--- dao/postgres.js
+++ dao/postgres.js
@@ -1,23 +1,27 @@
 const  pgp  = require('pg-promise')({noWarnings: true}); 
 const conf = require('../utils/config');
 
+let ssl = null;
+if (conf.postgres.ssl) {
+   ssl = {rejectUnauthorized: false};
+}
+
 var pool = {
     user: conf.postgres.user,
     host: conf.postgres.host,
     database: conf.postgres.database,
     password: conf.postgres.password,
     port: conf.postgres.port,
-    ssl: conf.postgres.ssl,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    }
+    ssl: ssl
   };
 
 function getDB() {
-  console.log(pool); 
+  try{
+    console.log(pool);
     return pgp(pool);
+  }catch(err){
+    console.log(err);
+  }
 }
 
 module.exports = {getDB};
